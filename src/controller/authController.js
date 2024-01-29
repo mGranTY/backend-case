afbf7bfe21ca2dd63407e7ceccea0e5143b434c2
GranTY <marcos.ricardo0203@gmail.com>
2024-01-29 11:07:14 -0300

    [FEAT] Auth tests

--- src/controller/authController.js
+++ src/controller/authController.js
@@ -51,8 +51,10 @@ export async function login(req, res){
         });
 
         res.status(200).json({session: session.sessionId, success: true});
+        return {session: session.sessionId, success: true}
     } catch (e) {
         Logging.error(e); // Log the error
-        res.status(500).json({message: e.message, success: false});
+        res.status(500).json({message: e.message, success: false})
+        return {message: e.message, success: false}
     }
 }
--- test/auth/auth.test.js
+++ test/auth/auth.test.js
@@ -1,4 +1,4 @@
-import {createAccount} from "../../src/controller/authController.js";
+import {createAccount, login} from "../../src/controller/authController.js";
 import {beforeAll, describe, expect, test, vi} from "vitest";
 import {MongoMemoryServer} from "mongodb-memory-server";
 import mongoose from "mongoose";
@@ -36,7 +36,7 @@ describe('createAccount', () => {
 
 
         const result = await createAccount(req, res);
-        expect(result.success).toBe(true);
+        expect(result.success).toBeTruthy();
         expect(result).toStrictEqual({message: "Account created!", success: true});
     });
 
@@ -58,6 +58,103 @@ describe('createAccount', () => {
         expect(result.success).toBe(false);
     });
 
+
+
+    test('Deveria retornar erro ao tentar criar conta com uma senha inválida', async () => {
+        const req = {
+            body: {
+                email: 'email@gmail.com',
+                password: '😂😂',
+            },
+        };
+
+        const res = {
+            success: vi.fn(),
+            message: vi.fn(),
+            status: vi.fn(() => res), // Return the res object when status is called
+            json: vi.fn(() => res)    // Return the res object when json is called
+        };
+        const result = await createAccount(req, res);
+        expect(result.success).toBe(false);
+    });
+
 });
 
 
+
+describe('login', () => {
+    test('Deveria entrar em uma conta com credenciais válidas', async () => {
+
+        const req = {
+            body: {
+                email: 'test@gmail.com',
+                password: 'senha123',
+            },
+        };
+
+        const res = {
+            success: vi.fn(),
+            message: vi.fn(),
+            status: vi.fn(() => res),
+            json: vi.fn(() => res)
+        };
+
+
+        const result = await login(req, res);
+        console.log(result.session.length)
+        expect(result.success).toBeTruthy();
+        expect(result.session).toBeDefined()
+        //Valid Session string length
+        expect(result.session.length).toBe(40);
+    });
+
+    test('Deveria retornar erro ao tentar entrar em uma conta com uma senha inválida', async () => {
+
+        const req = {
+            body: {
+                email: 'test@gmail.com',
+                password: 'senha12',
+            },
+        };
+
+        const res = {
+            success: vi.fn(),
+            message: vi.fn(),
+            status: vi.fn(() => res),
+            json: vi.fn(() => res)
+        };
+
+
+        const result = await login(req, res);
+        expect(result.success).toBeFalsy();
+        expect(result.session).toBeUndefined()
+        expect(result.message).toBeDefined()
+        expect(result.message).toBe("AUTH_INVALID_PASSWORD")
+    })
+
+    test('Deveria retornar erro ao tentar entrar em uma conta com um email/key inválida', async () => {
+
+        const req = {
+            body: {
+                email: 'test2@gmail.com',
+                password: 'senha123',
+            },
+        };
+
+        const res = {
+            success: vi.fn(),
+            message: vi.fn(),
+            status: vi.fn(() => res),
+            json: vi.fn(() => res)
+        };
+
+
+        const result = await login(req, res);
+        console.log(result.message)
+        expect(result.success).toBeFalsy();
+        expect(result.session).toBeUndefined()
+        expect(result.message).toBeDefined()
+        expect(result.message).toBe("AUTH_INVALID_KEY_ID")
+    })
+
+});
